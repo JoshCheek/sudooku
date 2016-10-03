f7d95d63b3827be55a4094b0b3d4708064d434da
Josh Cheek <josh.cheek@gmail.com>
2016-10-03 04:04:36 -0500

    Solves normal boring maps

--- lib/sudoku.rb
+++ lib/sudoku.rb
@@ -2,11 +2,18 @@ class Sudoku
   InvalidBoard = Class.new RuntimeError
 
   def self.from_string(board_string)
-    board = board_string.gsub(" ", "")
-                        .gsub("\n\n", "\n")
-                        .lines.map { |line|
-      line.chars.tap(&:pop).tap { |chars| validate! chars }
-    }
+    board_string = board_string.gsub(" ", "").gsub("\n\n", "\n")
+    invalid_chars = board_string.gsub(/[_1-9\n]/, '').chars.uniq
+    if invalid_chars.any?
+      raise InvalidBoard, "#{invalid_chars.inspect} are not valid board characters"
+    end
+    board = board_string.lines.map do |line|
+      line.chars.tap(&:pop).tap do |chars|
+        validate! chars.map! { |char|
+          char == '_' ? nil : char.to_i
+        }
+      end
+    end
     board.transpose.each { |col| validate! col }
     board.each_slice(3) do |rows|
       rows.map { |row| row.each_slice(3).to_a }
@@ -17,16 +24,12 @@ class Sudoku
   end
 
   def self.validate!(row)
-    row.reject { |c| c == '_' }
+    row.reject { |c| c == nil }
        .group_by(&:itself)
        .map(&:last)
        .map(&:length)
        .reject { |count| count == 1 }
        .each { raise InvalidBoard }
-    invalid_chars = row.reject { |c| c =~ /[_1-9]/ }
-    if invalid_chars.any?
-      raise InvalidBoard, "#{invalid_chars.inspect} are not valid board characters"
-    end
     row
   end
 
@@ -35,5 +38,75 @@ class Sudoku
 
   def initialize(board)
     self.board = board
+  end
+
+  def solve!
+    return true if solved?
+    puts "\n#{self}"
+    # if STDIN.gets == "y\n"
+    #   require "pry"
+    #   binding.pry
+    # end
+    each_unsolved do |y, x|
+      potentials = potentials_at(y, x)
+      # p [y, x] => potentials
+      return false if potentials.empty?
+      potentials.each do |potential|
+        set y, x, potential
+        return true if solve!
+      end
+      set y, x, nil
+    end
+    solved?
+  end
+
+  def at(y, x)
+    board[y][x]
+  end
+
+  def set(y, x, value)
+    board[y][x] = value
+  end
+
+  AVAILABLE = [*1..9].freeze
+  def potentials_at(y, x)
+    values = AVAILABLE.dup
+    row(y).each      { |val| values.delete val }
+    col(x).each      { |val| values.delete val }
+    block(y, x).each { |val| values.delete val }
+    values
+  end
+
+  def each_unsolved
+    board.each_with_index do |row, y|
+      row.each_with_index do |value, x|
+        yield y, x unless value
+      end
+    end
+  end
+
+  def solved?
+    board.all? { |row| row.all? &:itself }
+  end
+
+  def row(y)
+    board[y]
+  end
+
+  def col(x)
+    board.transpose[x]
+  end
+
+  def block(y, x)
+    slice = board.each_slice(3).to_a[y/3]
+    slice.flat_map do |line|
+      line.each_slice(3).to_a[x/3]
+    end
+  end
+
+  def to_s
+    board.map do |row|
+      row.map { |val| val ? val : " " }.join
+    end.join("\n")
   end
 end
--- spec/sudoku_spec.rb
+++ spec/sudoku_spec.rb
@@ -58,51 +58,91 @@ RSpec.describe 'sudoku' do
       b[10, 0] = "\n"
       assert_valid board
       sudoku = Sudoku.from_string(b)
-      expect(sudoku.at 0, 0).to eq '5'
-      expect(sudoku.at 1, 0).to eq '6'
+      expect(sudoku.at 0, 0).to eq 5
+      expect(sudoku.at 1, 0).to eq 6
     end
   end
 
   describe 'solving' do
+    def assert_solves(board, solution)
+      sudoku = Sudoku.from_string(board)
+      expect(sudoku.solve!).to eq true
+      expect(sudoku.to_s).to eq Sudoku.from_string(solution).to_s
+    end
+
     it 'can solve by a missing piece horizontally' do
-      assert_solves <<~SUDOKU, 5, y: 0, x: 0
+      assert_solves <<~SUDOKU, <<~SUDOKU
         _34678912
-        _________
-        _________
-        _________
-        _________
-        _________
-        _________
-        _________
-        _________
+        _72195348
+        _98342567
+        _59761423
+        _26853791
+        _13924856
+        _61537284
+        _87419635
+        _45286179
+      SUDOKU
+        534678912
+        672195348
+        198342567
+        859761423
+        426853791
+        713924856
+        961537284
+        287419635
+        345286179
       SUDOKU
     end
 
     it 'can solve by a missing piece vertically' do
-      assert_solves <<~SUDOKU, 5, y: 0, x: 0
+      assert_solves <<~SUDOKU, <<~SUDOKU
        _________
-       6________
-       1________
-       8________
-       4________
-       7________
-       9________
-       2________
-       3________
-     SUDOKU
+       672195348
+       198342567
+       859761423
+       426853791
+       713924856
+       961537284
+       287419635
+       345286179
+      SUDOKU
+        534678912
+        672195348
+        198342567
+        859761423
+        426853791
+        713924856
+        961537284
+        287419635
+        345286179
+      SUDOKU
     end
 
     it 'can solve by a missing piece in the block' do
-      assert_solves <<~SUDOKU, 5, y: 0, x: 0
-        _34______
-        672______
-        198______
-        _________
-        _________
-        _________
-        _________
-        _________
-        _________
+      sudoku = assert_solves <<~SUDOKU, <<~SUDOKU
+        _34 _78 _12
+        672 195 348
+        198 342 567
+
+        _59 _61 _23
+        426 853 791
+        713 924 856
+
+        _61 _37 _84
+        287 419 635
+        345 286 179
+      SUDOKU
+        534 678 912
+        672 195 348
+        198 342 567
+
+        859 761 423
+        426 853 791
+        713 924 856
+
+        961 537 284
+        287 419 635
+        345 286 179
       SUDOKU
     end
 
@@ -110,6 +150,23 @@ RSpec.describe 'sudoku' do
     it 'can solve by a conjunction of missing pieces horizontally by block'
     it 'can solve by a conjunction of missing pieces vertically by block'
     it 'can solve by a conjunction of missing pieces horizontally, vertically, and by block'
+
+    xit 'can solve the hard problem given in the video', t:true do
+      sudoku = Sudoku.from_string <<~SUDOKU
+        8__ ___ ___
+        __3 6__ ___
+        _7_ _9_ 2__
+
+        _5_ __7 ___
+        ___ _45 7__
+        ___ 1__ _3_
+
+        __1 ___ _68
+        __8 5__ _1_
+        _9_ ___ 4__
+      SUDOKU
+      expect(sudoku.solve!).to eq true
+    end
   end
 end
 
